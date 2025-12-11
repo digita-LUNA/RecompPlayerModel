5ee0b63b4f102c2776f510e78ac92cba9cbb2e23
MoriyaFaith <46070717+digita-LUNA@users.noreply.github.com>
2025-12-10 22:58:17 -0500

    first version

--- src/main.c
+++ src/main.c
@@ -3,26 +3,19 @@
 #include "recomputils.h"
 #include "recompconfig.h"
 #include "playermodelmanager_api.h"
+#include "digita_link.h"
 #include "kafei.h"
 
-PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerKafei() {
+PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerDigitaLink() {
     setupKafeiAssets();
 
     // PlayerModelManager stuff
-    PlayerModelManagerHandle h = PLAYERMODELMANAGER_REGISTER_MODEL("mm_kafei_pmm_built_in", PMM_MODEL_TYPE_CHILD);
+    PlayerModelManagerHandle h = PLAYERMODELMANAGER_REGISTER_MODEL("mm_digitalink_pmm", PMM_MODEL_TYPE_CHILD);
 
-    PlayerModelManager_setAuthor(h, "Nintendo");
-    PlayerModelManager_setDisplayName(h, "Kafei");
-    PlayerModelManager_setEyesTextures(h, gKafeiEyeTextures);
-    PlayerModelManager_setMouthTextures(h, gKafeiMouthTextures);
+    PlayerModelManager_setAuthor(h, "digita-LUNA");
+    PlayerModelManager_setDisplayName(h, "Updated Link");
+    PlayerModelManager_setEyesTextures(h, gLinkHumanEyeTextures);
+    PlayerModelManager_setMouthTextures(h, gLinkHumanMouthTextures);
 
-    PlayerModelManager_setSkeleton(h, &gKafeiSkeleton);
-
-    PlayerModelManager_setDisplayList(h, PMM_DL_LFIST, gKafeiLeftFist);
-    PlayerModelManager_setDisplayList(h, PMM_DL_RFIST, gKafeiRightFist);
-    PlayerModelManager_setDisplayList(h, PMM_DL_LHAND_BOTTLE, gKafeiLeftHandBottle);
-    PlayerModelManager_setDisplayList(h, PMM_DL_FPS_RHAND, gKafeiFPSRightHand);
-    PlayerModelManager_setDisplayList(h, PMM_DL_FPS_RFOREARM, gKafeiFPSRightForearm);
-    PlayerModelManager_setDisplayList(h, PMM_DL_FPS_LHAND, gKafeiFPSLeftHand);
-    PlayerModelManager_setDisplayList(h, PMM_DL_FPS_LFOREARM, gKafeiFPSLeftForearm);
+    PlayerModelManager_setSkeleton(h, &gLinkHumanSkel);
 }
